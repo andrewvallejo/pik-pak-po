c6301504da1dbe6160caca008f4868c92684fec9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-04-22 21:34:05 -0600

    Added IDs to all of the battlecard's elements. Added and tested mediaqueries to each of those moves.

---
+++ javaScript/main.js
@@ -0,0 +1,30 @@
+// Battle Card
+var swipeMove =  document.querySelector('#swipeMove')
+var hardenMove = document.querySelector('#hardenMove')
+var abilityMove = document.querySelector('#abilityMove')
+var bagMove = document.querySelector('#bagMove')
+var runMove = document.querySelector('#runMove')
+
+swipeMove.addEventListener('click', saySwipe)
+hardenMove.addEventListener('click', sayHarden)
+abilityMove.addEventListener('click', sayAbil)
+bagMove.addEventListener('click', sayBag)
+runMove.addEventListener('click', sayRun)
+
+
+
+function saySwipe() {
+  console.log('swipe')
+}
+function sayHarden() {
+  console.log('harden')
+}
+function sayAbil() {
+  console.log('abil')
+}
+function sayBag() {
+  console.log('bag')
+}
+function sayRun() {
+  console.log('run')
+}
