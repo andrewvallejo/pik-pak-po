50745007167fe7bf5d51aee4e0bc762607719cb8
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-04-24 10:08:53 -0600

    Fixed bug made from creating the opening card

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,10 +1,13 @@
-// Battle Card
+// Card
+var outcomeMsg = document.querySelector('#outcome')
 var swipeMove =  document.querySelector('#swipeMove')
 var hardenMove = document.querySelector('#hardenMove')
 var abilityMove = document.querySelector('#abilityMove')
 var bagMove = document.querySelector('#bagMove')
 var runMove = document.querySelector('#runMove')
-var outcomeMsg = document.querySelector('#outcome')
+var fightMove = document.querySelector('#fightMove')
+var openRunMove = document.querySelector('#openRunMove')
+
 
 // Battle 
 swipeMove.addEventListener('click', swipe)
