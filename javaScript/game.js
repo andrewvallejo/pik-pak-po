7eee9d017ca4c9fa807ae34c84355aae5a1aaebc
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-04-26 13:55:42 -0600

    Add win count to game.js

--- javaScript/game.js
+++ javaScript/game.js
@@ -5,6 +5,8 @@ class Game {
    this.playerMove = ''
    this.gymleaderMove = ''
    this.winner = ''
+   this.playersWins = 0
+   this.gymleaderWins = 0
   }
   updatePlayerMove(move) {
     this.playerMove = move
@@ -25,7 +27,7 @@ class Game {
 
   getOutcome() {
     var outcome = this.compareMove(this.playerMove, this.gymleaderMove)
-    outcome === 'win' ? this.player.battlesWon++ : null
+    outcome === 'win' ? this.player.wins++ : null
     outcome === 'win' ? outcomeMsg.innerText = 'You Win!' :
     outcome === 'lose' ? outcomeMsg.innerText = 'You Lose!':
     outcome === 'draw' ? outcomeMsg.innerText = 'Draw!' :
